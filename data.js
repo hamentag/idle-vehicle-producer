a738fbd2e5f295c29d44ce9a6499f4511d11ef8e
Hamza Amentag <hamentag1@gmail.com>
2023-12-08 19:16:49 -0600

    Updated the Prodcut class design

--- data.js
+++ data.js
@@ -1,265 +1,264 @@
-window.data = {
-    car: {
-        count: 0,               // car count
-        totalUPPUT: 0,          // total units produced per unit time
-        icon: '🚘',
-
-        producers: [            // car producers
-        {
+window.data = [
+  {                 // product 1
+    name: 'car',
+    count: 0,               // car count
+    totalUPPUT: 0,          // total units produced per unit time
+    icon: '🚘',
+    producers: [          
+      {
         id: 'perfect_motors',
         price: 4,
         unlocked: false,
         upput: 1,               // units produced per unit time
         qty: 0
-        },
-        {
+      },
+      {
         id: 'germany_auto',
         price: 10,
         unlocked: false,
         upput: 2,
         qty: 0
-        },
-        {
+      },
+      {
         id: 'mr._power',
         price: 20,
         unlocked: false,
         upput: 5,
         qty: 0
-        },
-        {
+      },
+      {
         id: 'top_runner',
         price: 50,
         unlocked: false,
         upput: 10,
         qty: 0
-        },
-        {
+      },
+      {
         id: 'efficient_motor',
         price: 100,
         unlocked: false,
         upput: 15,
         qty: 0
-        },
-        {
+      },
+      {
         id: 'tadla_vehicle',
         price: 200,
         unlocked: false,
         upput: 20,
         qty: 0
-        },
-        {
+      },
+      {
         id: 'tm23_runner',
         price: 300,
         unlocked: false,
         upput: 30,
         qty: 0
-        },
-        {
+      },
+      {
         id: 'car_factory',
         price: 500,
         unlocked: false,
         upput: 50,
         qty: 0
-        },
-        {
+      },
+      {
         id: 'strong_torque',
         price: 1000,
         unlocked: false,
         upput: 100,
         qty: 0
-        },
-        {
+      },
+      {
         id: 'energetic_car',
         price: 1500,
         unlocked: false,
         upput: 150,
         qty: 0
-        },
-        {
+      },
+      {
         id: 'car_planet',
         price: 2000,
         unlocked: false,
         upput: 200,
         qty: 0
-        },
-        {
+      },
+      {
         id: 'high_speed',
         price: 2500,
         unlocked: false,
         upput: 250,
         qty: 0
-        }
+      }      
     ],
-    },
-
-    robot: {
-        count: 0,            // robot count
-        totalUPPUT: 0,       // total units produced per unit time
-        icon: '🤖',
-
-        producers: [         // robot producers
-        {
-          id: 'dynamic',
-          price: 6,
-          unlocked: false,
-          upput: 1,          // units produced per unit time
-          qty: 0
-        },
-        {
-          id: 'robot_pro',
-          price: 10,
-          unlocked: false,
-          upput: 2,
-          qty: 0
-        },
-        {
-          id: 'mr._robot',
-          price: 15,
-          unlocked: false,
-          upput: 3,
-          qty: 0
-        },
-        {
-          id: 'robotic',
-          price: 20,
-          unlocked: false,
-          upput: 4,
-          qty: 0
-        },
-        {
-          id: 'alpha_dynamic',
-          price: 30,
-          unlocked: false,
-          upput: 6,
-          qty: 0
-        },
-        {
-          id: 'mc2_top',
-          price: 50,
-          unlocked: false,
-          upput: 10,
-          qty: 0
-        },
-        {
-          id: 'digital_pro',
-          price: 100,
-          unlocked: false,
-          upput: 50,
-          qty: 0
-        },
-        {
-          id: 'core_tech',
-          price: 150,
-          unlocked: false,
-          upput: 75,
-          qty: 0
-        },
-        {
-          id: 'robot_prime',
-          price: 200,
-          unlocked: false,
-          upput: 80,
-          qty: 0
-        },
-        {
-          id: 'techno_core',
-          price: 300,
-          unlocked: false,
-          upput: 120,
-          qty: 0
-        },
-        {
-          id: 'super_robotic',
-          price: 450,
-          unlocked: false,
-          upput: 200,
-          qty: 0
-        },
-        {
-          id: 'robot_planet',
-          price: 500,
-          unlocked: false,
-          upput: 250,
-          qty: 0
-        }
-      ],
-    },
-    truck: {
-        count: 0,            // truck count
-        totalUPPUT: 0,       // total units produced per unit time
-        icon: '🚚',
-
-        producers: [         // truck producers
-        {
-          id: 'top_truck',
-          price: 6,
-          unlocked: false,
-          upput: 1,          // units produced per unit time
-          qty: 0
-        },
-        {
-          id: 'mr._truck',
-          price: 10,
-          unlocked: false,
-          upput: 2,
-          qty: 0
-        },
-        {
-          id: 'truck_pro',
-          price: 15,
-          unlocked: false,
-          upput: 3,
-          qty: 0
-        },
-        {
-          id: 'tr_dynamic',
-          price: 20,
-          unlocked: false,
-          upput: 5,
-          qty: 0
-        },
-        {
-          id: 'alpha_trucks',
-          price: 30,
-          unlocked: false,
-          upput: 10,
-          qty: 0
-        },
-        {
-          id: 'mech_tr',
-          price: 45,
-          unlocked: false,
-          upput: 12,
-          qty: 0
-        },
-        {
-          id: 'digital_pro',
-          price: 80,
-          unlocked: false,
-          upput: 45,
-          qty: 0
-        },
-        {
-          id: 'tech_trucks',
-          price: 130,
-          unlocked: false,
-          upput: 60,
-          qty: 0
-        },
-        {
-          id: 'truck_planet',
-          price: 180,
-          unlocked: false,
-          upput: 80,
-          qty: 0
-        },
-        {
-          id: 'super_truck',
-          price: 200,
-          unlocked: false,
-          upput: 120,
-          qty: 0
-        }
-      ],
-    },
-}
+  },
+  {                   // product 2
+    name: 'robot',
+    count: 0,            // robot count
+    totalUPPUT: 0,       // total units produced per unit time
+    icon: '🤖',
+    producers: [          // robot producers
+      {
+        id: 'dynamic',
+        price: 6,
+        unlocked: false,
+        upput: 1,          // units produced per unit time
+        qty: 0
+      },
+      {
+        id: 'robot_pro',
+        price: 10,
+        unlocked: false,
+        upput: 2,
+        qty: 0
+      },
+      {
+        id: 'mr._robot',
+        price: 15,
+        unlocked: false,
+        upput: 3,
+        qty: 0
+      },
+      {
+        id: 'robotic',
+        price: 20,
+        unlocked: false,
+        upput: 4,
+        qty: 0
+      },
+      {
+        id: 'alpha_dynamic',
+        price: 30,
+        unlocked: false,
+        upput: 6,
+        qty: 0
+      },
+      {
+        id: 'mc2_top',
+        price: 50,
+        unlocked: false,
+        upput: 10,
+        qty: 0
+      },
+      {
+        id: 'digital_pro',
+        price: 100,
+        unlocked: false,
+        upput: 50,
+        qty: 0
+      },
+      {
+        id: 'core_tech',
+        price: 150,
+        unlocked: false,
+        upput: 75,
+        qty: 0
+      },
+      {
+        id: 'robot_prime',
+        price: 200,
+        unlocked: false,
+        upput: 80,
+        qty: 0
+      },
+      {
+        id: 'techno_core',
+        price: 300,
+        unlocked: false,
+        upput: 120,
+        qty: 0
+      },
+      {
+        id: 'super_robotic',
+        price: 450,
+        unlocked: false,
+        upput: 200,
+        qty: 0
+      },
+      {
+        id: 'robot_planet',
+        price: 500,
+        unlocked: false,
+        upput: 250,
+        qty: 0
+      }
+    ],
+  },
+  {                 // product 3
+    name: 'truck',
+    count: 0,            // truck count
+    totalUPPUT: 0,       // total units produced per unit time  
+    icon: '🚚',
+    producers: [          // truck producers
+      {
+        id: 'top_truck',
+        price: 6,
+        unlocked: false,
+        upput: 1,          // units produced per unit time
+        qty: 0
+      },
+      {
+        id: 'mr._truck',
+        price: 10,
+        unlocked: false,
+        upput: 2,
+        qty: 0
+      },
+      {
+        id: 'truck_pro',
+        price: 15,
+        unlocked: false,
+        upput: 3,
+        qty: 0
+      },
+      {
+        id: 'tr_dynamic',
+        price: 20,
+        unlocked: false,
+        upput: 5,
+        qty: 0
+      },
+      {
+        id: 'alpha_trucks',
+        price: 30,
+        unlocked: false,
+        upput: 10,
+        qty: 0
+      },
+      {
+        id: 'mech_tr',
+        price: 45,
+        unlocked: false,
+        upput: 12,
+        qty: 0
+      },
+      {
+        id: 'digital_pro',
+        price: 80,
+        unlocked: false,
+        upput: 45,
+        qty: 0
+      },
+      {
+        id: 'tech_trucks',
+        price: 130,
+        unlocked: false,
+        upput: 60,
+        qty: 0
+      },
+      {
+        id: 'truck_planet',
+        price: 180,
+        unlocked: false,
+        upput: 80,
+        qty: 0
+      },
+      {
+        id: 'super_truck',
+        price: 200,
+        unlocked: false,
+        upput: 120,
+        qty: 0
+      }
+    ],
+  },
+]
--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 // Bring in the data object from the data.js file
 const data = window.data;
 
+// State
+const state = {
+  products: [],
+};
+
 // References
 const mainContainer = document.getElementById('main-container');
 
-// Classes declaration
+// Class declaration
 class Product {
-  constructor(_name, _product){
-    this.name = _name;
-    this.count = _product.count;            // product count
-    this.totalUPPUT = _product.totalUPPUT;  // total units produced per unit time
-    this.icon = _product.icon;
+  constructor(_name, _count, _totalUPPUT, _icon, _producers){
+    this.name = _name;              // product name
+    this.count = _count;            // product count
+    this.totalUPPUT = _totalUPPUT;  // total units produced per unit time
+    this.icon = _icon;
+    this.producers = _producers;
   }
-  getCount() { return this.count; }
 
   updateDisplayedCount(){
     const productCounter = document.getElementById(`${this.name}-counter`);
@@ -23,168 +28,151 @@ class Product {
     const upputDiv = document.getElementById(`${this.name}-UPPUT`);
     upputDiv.innerText = this.totalUPPUT;
   }
-  clickProduct(){
-    this.count += 1;
-    this.updateDisplayedCount();
-  }
-
-  deductCost(cost){
-    this.count -= cost; 
-  }
   
   increaseThroughput(increase){
     this.totalUPPUT += increase;
     this.updateDisplayedTotalUPPUT();
   }
-}
 
-class Producer { 
-  constructor(_name, _producer){
-    this.name = _name;
-    this.id = _producer.id;
-    this.price = _producer.price;
-    this.unlocked = _producer.unlocked;
-    this.upput = _producer.upput;
-    this.qty = _producer.qty;
+  clickProduct(){
+    this.count += 1;
+    this.updateDisplayedCount();
   }
 
-  setUnlocked(count){
-    if(count >= this.price / 2){
-      this.unlocked = true;
-    }
+  periodicProduction(){
+    this.count += this.totalUPPUT;
+    this.updateDisplayedCount();
+    this.renderProducers();
   }
 
-  makeDisplayNameFromId() {
-    return this.id.split("_")
+  deductCost(cost){
+    this.count -= cost; 
+  }
+
+  setUnlocked(){
+    this.producers.map(producer => {
+      if(this.count >= producer.price / 2)
+      producer.unlocked = true;
+    });
+  }
+  
+  formatName(string) {
+    return string.split("_")
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
       .join(" ");
   }
 
   makeProducerDiv() {
-    if(this.unlocked){
-      const containerDiv = document.createElement("div");
-      containerDiv.className = "producer";
-      if(this.qty > 0){
-        containerDiv.classList.add('bought');
+    return this.producers.map(producer => {
+      if(producer.unlocked){
+        const containerDiv = document.createElement("div");
+        containerDiv.className = "producer";
+        if(producer.qty > 0)
+          containerDiv.classList.add('bought');
+        const producerHtml = `
+        <div class="producer-column">
+          <div class="producer-name">${this.formatName(producer.id)}</div>
+        </div>
+        <div class="producer-column">
+          <div>Quantity: ${producer.qty}</div>
+          <div>Unit/second: ${producer.upput}</div>
+          <div>Cost: ${producer.price} Unit</div>
+        </div>
+        `;
+        containerDiv.innerHTML = producerHtml;
+        const buyBtn = document.createElement('button');
+        buyBtn.textContent = 'Buy';
+        buyBtn.className = `${this.name}_button`;
+        buyBtn.id = `buy_${producer.id}`;
+        buyBtn.addEventListener('click', (event) => this.buyBtnClick(event));
+        containerDiv.append(buyBtn);       
+        return containerDiv;
       }
-      const producerHtml = `
-        <div class="producer-column">
-          <div class="producer-name">${this.makeDisplayNameFromId()}</div>
-        </div>
-        <div class="producer-column">
-          <div>Quantity: ${this.qty}</div>
-          <div>Unit/second: ${this.upput}</div>
-          <div>Cost: ${this.price} Unit</div>
-        </div>
-      `;
-      containerDiv.innerHTML = producerHtml;
-      const buyBtn = document.createElement('button');
-      buyBtn.textContent = 'Buy';
-      buyBtn.className = `${this.name}_button`;
-      buyBtn.id = `buy_${this.id}`;
-      buyBtn.addEventListener('click', (event) => buyBtnClick(event,this.name));
-      containerDiv.append(buyBtn);       
-
-      return containerDiv;
-    }
-    return '';    
+      return '';
+    });
   }
-  attemptToBuyProducer(product){
-    if(this.price <= product.getCount()){  
-        product.deductCost(this.price);
-        this.qty += 1;
-        this.price = Math.floor(this.price * 1.25);
-        product.increaseThroughput(this.upput);
+
+  // Handling buy buttons click events 
+  buyBtnClick(event){
+    const producerId = event.target.id.slice(4);
+    const selectedProducer = this.producers.find(producer => producer.id === producerId);
+    if(selectedProducer.price <= this.count){  
+      this.deductCost(selectedProducer.price);
+      selectedProducer.qty += 1;
+      selectedProducer.price = Math.floor(selectedProducer.price * 1.25);
+      this.increaseThroughput(selectedProducer.upput);
+
+      this.renderProducers();
     }
     else{
       window.alert("Not enough produced units!");      
-    }  
-  }
-}
-
-//// Handling buy button events 
-function buyBtnClick(event, name){
-    const producerId = event.target.id.slice(4);
-    for(let index in products){
-        
-        if(products[index].name === name){
-            const selectedProducer = producers[index].find(producer => producer.id === producerId);
-            selectedProducer.attemptToBuyProducer(products[index]);
-        }
-    }
-    render();
+    } 
   }
 
-///////
-function render(){
-  products.forEach((product, index) => {
-    const producersCard = document.getElementById(`${product.name}-producers`);
-    const producersDivs = producers[index].map(producer => {
-        producer.setUnlocked(product.getCount());
-        return producer.makeProducerDiv();
-    });
+  renderProducers(){
+    const producersCard = document.getElementById(`${this.name}-producers`);
+    this.setUnlocked();      
+    const producersDivs = this.makeProducerDiv();
     producersCard.replaceChildren(...producersDivs);
-  });
-}
+  } 
 
-//
-function capitalize(word){
-  // return toUpperCase(word[0]) + word.slice(1);
-  return word.charAt(0).toUpperCase() + word.slice(1);
-}
+  setHtml(){
+    const productContainer = document.createElement('div');
+    productContainer.className = 'product-container';
 
-////
-function setHtml(){
-    const productContainers = products.map(product => {
-        const productContainer = document.createElement('div');
-        productContainer.className = 'product-container';
-
-    const productHtml = `
-      <div class="top">
-          <div class="counter-container">${capitalize(product.name)}: <span id="${product.name}-counter">0</span></div>
-          <div class="UPPUT-container"><span id="${product.name}-UPPUT">0</span> ${product.name}/month</div>
-          <div class="icon" id="${product.name}-iicon">${product.icon}</div>
-      </div>
-      <div class="producers-title">${capitalize(product.name)} Producers</div>
-      <div class="bottom" id="${product.name}-producers"></div>
-      `;
-    productContainer.innerHTML = productHtml;
-    return productContainer;
-    });
-    mainContainer.replaceChildren(... productContainers);
-}
-
-/// Create Product and Producer instances
-const products = [];
-const producers = [];
-for(let key in data){
-    products.push(new Product(key, data[key]));
-    const prtProducers = data[key].producers.map(producer => {
-        return new Producer(key, producer);
-    });
-    producers.push(prtProducers);
-}
-
-// init 
-setHtml();
-
-// Handling icon click events 
-products.forEach(product => {
-    const iconEl = document.getElementById(`${product.name}-iicon`);
+    const iconEl = document.createElement('div')
+    iconEl.className = 'icon'
+    iconEl.id = `${this.name}-icon`;
+    iconEl.textContent = this.icon;
     iconEl.addEventListener('click', () => {
-        product.clickProduct();
-        render();
+      this.clickProduct();
+      this.renderProducers();
     });
-});
 
-//// cycle
-function tick() {
-    products.forEach(product => {
-        product.count += product.totalUPPUT;
-        product.updateDisplayedCount();
-    });
-  render();
+    const indicators = document.createElement('div')
+    indicators.innerHTML =  `
+    <div class="indicators">
+        <div class="counter-container">${this.formatName(this.name)}: <span id="${this.name}-counter">0</span></div>
+        <div class="UPPUT-container"><span id="${this.name}-UPPUT">0</span> ${this.name}/month</div>
+    </div>
+    `;
+
+    const top = document.createElement('div')
+    top.className = 'top'
+    top.append(iconEl, indicators);
+
+    const producersTitle = document.createElement('div')
+    producersTitle.className = 'producers-title'
+    producersTitle.textContent = `${this.formatName(this.name)} Producers`
+
+    const bottom = document.createElement('div')
+    bottom.className = 'bottom'
+    bottom.id = `${this.name}-producers` 
+
+    productContainer.append(top, producersTitle, bottom);
+
+    return productContainer;
+  }
 }
 
-// Rpeat the tick function every 2000ms, or 2s
-setInterval(() => tick(), 2000);
+// Set up the initial state
+function init(){
+  // Create Product instances
+  data.forEach(el => state.products.push(Object.setPrototypeOf(el, Product.prototype)));
+  // OR to preserve data as it is:
+  // data.forEach(el => state.products.push(new Product(el.name, el.count, el.totalUPPUT,el.icon, el.producers) ));
+
+  // set HTML
+  const productContainers = state.products.map(product => product.setHtml());
+  mainContainer.replaceChildren(... productContainers);
+}
+
+init();
+
+// Periodic step
+function tick() {
+    state.products.forEach(product => product.periodicProduction());
+}
+
+// Rpeat the tick function every 1000ms, or 1s
+setInterval(() => tick(), 1000);
